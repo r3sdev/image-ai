3117cc7ee1a692cd3dbbf95d2e6d596761d0f8a6
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-30 08:44:38 +0100

    chore: run initial training with cifar10

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # Code taken from https://stackabuse.com/image-recognition-in-python-with-tensorflow-and-keras/
 
 import numpy
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.constraints import maxnorm
 from keras.utils import np_utils
+import tensorflowjs as tfjs
 from keras.datasets import cifar10
+# from keras.datasets import cifar100
+
+MODEL_NAME = "image-classification"
 
 # Set random seed for purposes of reproducibility
 seed = 21
@@ -25,62 +29,68 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 class_num = y_test.shape[1]
 
-model = Sequential()
+model = load_model(MODEL_NAME)
 
-model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
-model.add(Activation('relu'))
+# model = Sequential()
 
-model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
+# model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+# model.add(Activation('relu'))
 
-model.add(Dropout(0.2))
+# model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
 
-model.add(BatchNormalization())
+# model.add(Dropout(0.2))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
+# model.add(BatchNormalization())
 
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
+# model.add(Conv2D(64, (3, 3), padding='same'))
+# model.add(Activation('relu'))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.2))
+# model.add(BatchNormalization())
+
+# model.add(Conv2D(64, (3, 3), padding='same'))
+# model.add(Activation('relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.2))
+# model.add(BatchNormalization())
     
-model.add(Conv2D(128, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
+# model.add(Conv2D(128, (3, 3), padding='same'))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(BatchNormalization())
 
-model.add(Flatten())
-model.add(Dropout(0.2))
+# model.add(Flatten())
+# model.add(Dropout(0.2))
 
-model.add(Dense(256, kernel_constraint=maxnorm(3)))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
+# model.add(Dense(256, kernel_constraint=maxnorm(3)))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(BatchNormalization())
     
-model.add(Dense(128, kernel_constraint=maxnorm(3)))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
+# model.add(Dense(128, kernel_constraint=maxnorm(3)))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(BatchNormalization())
 
-model.add(Dense(class_num))
-model.add(Activation('softmax'))
+# model.add(Dense(class_num))
+# model.add(Activation('softmax'))
 
-epochs = 25
-optimizer = 'adam'
+# epochs = 25
+# optimizer = 'adam'
 
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+# model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-print(model.summary())
+# print(model.summary())
 
 
-numpy.random.seed(seed)
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
+# numpy.random.seed(seed)
+# model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
 
 # Model evaluation
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+# scores = model.evaluate(X_test, y_test, verbose=0)
+# print("Accuracy: %.2f%%" % (scores[1]*100))
+# model.save("image-classification")
+# tfjs.converters.save_keras_model(model, "tfjsmodel")
+
+print(model.summary())
